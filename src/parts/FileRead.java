83008435306a861f93446ddfb49dad98fb69e941
Danny Cheng <feng.cheng@kaplan.com>
2015-03-26 17:36:36 -0400

    New way to read in file & static import
    Java 7 JDK read in file

--- src/parts/FileRead.java
+++ src/parts/FileRead.java
@@ -1,19 +1,33 @@
 package parts;
 
+//Static import 
+import static java.lang.System.out;
+
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 
 class FileRead
 {
 	public static void main(String args[])
 	{
+		String filePath = "C:\\development\\apache-tomcat-6.0.26\\apache-tomcat-6.0.26\\webapps\\pc-content\\trunk\\xml\\ip\\R\\SAT\\SA08\\R-SAT-SA08-P009.xml";
+		FileRead read = new FileRead();
+		read.oldReadFile(filePath);
+	}
+
+	public void oldReadFile(String path)
+	{
 		try
 		{
 			// Open the file that is the first
 			// command line parameter
-			FileInputStream fstream = new FileInputStream("C:\\development\\apache-tomcat-6.0.26\\apache-tomcat-6.0.26\\webapps\\pc-content\\trunk\\xml\\ip\\R\\SAT\\SA08\\R-SAT-SA08-P009.xml");
+			FileInputStream fstream = new FileInputStream(path);
 			// Get the object of DataInputStream
 			DataInputStream in = new DataInputStream(fstream);
 			BufferedReader br = new BufferedReader(new InputStreamReader(in));
@@ -31,5 +45,14 @@ class FileRead
 		{// Catch exception if any
 			System.err.println("Error: " + e.getMessage());
 		}
+	}
+
+	/*
+	 * java 7 reading file
+	 */
+	public void jdk7ReadFile(String path) throws IOException
+	{
+		out.println(new String(Files.readAllBytes(Paths.get("data_old.txt"))));
+		out.println(new String(Files.readAllBytes(Paths.get("info.xml"))));
 	}
 }
